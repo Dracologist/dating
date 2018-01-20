924248851be8024bb7de59e3bac47a73fe4df0aa
Dracologist <ejckanzler@yahoo.com>
2018-01-20 00:58:31 -0500

    created home page, added a stylesheet, and uploaded an image for the home page

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require_once('vendor/autoload.php');
+$f3 = Base::instance();
+
+$f3->route('GET /', function() {
+    $view = new View;    echo $view->render
+    ('pages/home.html');
+});
+
+$f3->run();
